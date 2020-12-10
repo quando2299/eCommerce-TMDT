dca7339a034b3f0acb364d53389c54614bc404ee
quando2299 <quando.2299@gmail.com>
2020-12-10 20:26:54 +0700

    Add Method Payment!

---
+++ BookBook/Models/OrderInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookBook.Models
+{
+    public class OrderInfo
+    {
+        public decimal OrderId { get; set; }
+        public decimal Amount { get; set; }
+        public string OrderDescription { get; set; }
+        public string BankCode { get; set; }
+        public int Status { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public decimal vnp_TransactionNo { get; set; }
+        public string vpn_Message { get; set; }
+        public string vpn_TxnResponseCode { get; set; }
+    }
+}
