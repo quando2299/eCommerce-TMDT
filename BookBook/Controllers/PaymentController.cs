4b30db0646af693b5513ae07db0fb03c697e9d31
quando2299 <quando.2299@gmail.com>
2020-12-13 01:27:43 +0700

    fix detail product, product by type and add rating, comment in product

---
+++ BookBook/Controllers/PaymentController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using log4net;
+
+namespace BookBook.Controllers
+{
+    public class PaymentController : Controller
+    {
+        private static readonly ILog log =
+          LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        // GET: Payment
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
